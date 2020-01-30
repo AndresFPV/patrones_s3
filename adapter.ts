8cd2689e771bc3a7e1699f361d1ccf5cfbbb399c
ANDRES PORRAS VERGARAY <andresporras@MacBook-Pro-de-ANDRES-2.local>
2020-01-30 03:17:47 -0500

    Primer commit ejemplos terminados, no habia git init antes

---
+++ adapter.ts
@@ -0,0 +1,81 @@
+//$ npm install sqlite3 @types/sqlite3 --save
+//$ npm install pouchdb --save
+
+import {Database} from "sqlite3";
+
+interface Alumno
+{
+    codigo : string
+    nombre : string 
+    carrera : string
+}
+
+abstract class BDAdapter
+{
+    abstract conectar()
+    abstract crearEstructura()
+    abstract insertarAlumno(alumno : Alumno)
+    abstract cerrar()
+}
+
+class SQLiteAdapter extends BDAdapter 
+{
+    //Es una propiedad propia del Adapter de SQLite
+    db : Database | null = null 
+
+    conectar()
+    {
+        this.db = new Database("alumnos.sqlite")
+    }
+
+    crearEstructura()
+    {
+        //DDL, el run es de la clase Database
+        this.db.run(`CREATE TABLE alumno (codigo TEXT, nombre TEXT, carrera TEXT)`)
+    }
+
+    insertarAlumno(alumno : Alumno)
+    {
+        //DML
+        this.db.run(`INSERT INTO alumno values 
+        ('${alumno.codigo}', '${alumno.nombre}', '${alumno.carrera}')`)
+    }
+
+    cerrar()
+    {
+        this.db.close()
+        this.db = null
+    }
+}
+
+class PouchDBAdapter extends BDAdapter
+{
+    conectar() {
+        throw new Error("Method not implemented.");
+    }    
+    crearEstructura() {
+        throw new Error("Method not implemented.");
+    }
+    insertarAlumno(alumno: Alumno) {
+        throw new Error("Method not implemented.");
+    }
+    cerrar() {
+        throw new Error("Method not implemented.");
+    }
+
+
+}
+
+let mainAdapter = () => {
+    let adapter : BDAdapter = new SQLiteAdapter()
+    adapter.conectar()
+    adapter.crearEstructura()
+    adapter.insertarAlumno({
+        nombre : "Pepito",
+        codigo : "20142153",
+        carrera : "Ingenieria de Sistemas"
+    })
+    adapter.cerrar()
+}
+
+mainAdapter()
---
+++ chain.ts
@@ -0,0 +1,77 @@
+interface Reclamo
+{
+    monto : number
+}
+
+abstract class Empleado
+{
+    sucesor : Empleado
+
+    abstract analizarReclamo(reclamo : Reclamo)
+
+    addSucesor(sucesor : Empleado)
+    {   
+        this.sucesor = sucesor
+    }
+}
+
+class Cajero extends Empleado
+{
+    analizarReclamo(reclamo : Reclamo)
+    {
+        if (reclamo.monto >= 300)
+        {
+            //No lo puede resolver y tiene que derivarlo
+            this.sucesor.analizarReclamo(reclamo)
+        }else{
+            console.log("El cajero resolvio el reclamo")
+        }
+
+    }
+}
+
+class Administrador extends Empleado
+{
+    analizarReclamo(reclamo : Reclamo)
+    {
+        if (reclamo.monto >= 1000)
+        {
+            //No lo puede resolver y tiene que derivarlo
+            this.sucesor.analizarReclamo(reclamo)
+        }else{
+            console.log("El administrador resolvio el reclamo")
+        }
+
+    }
+}
+
+class Gerente extends Empleado
+{
+    analizarReclamo(reclamo : Reclamo)
+    {
+        console.log("El problema se ha resuleto")
+    }
+}
+
+let mainChain = () => {
+    //1. Definir la estructura de mi cadena de responsabilidad
+    let cajero = new Cajero()
+    let administrador = new Administrador()
+    let gerente = new Gerente()
+
+    cajero.addSucesor(administrador)
+    administrador.addSucesor(gerente)
+
+    //2. Ejecutar la peticion
+    let rec1 : Reclamo = {
+        monto : 150
+    }
+    cajero.analizarReclamo(rec1)
+
+    let rec2 : Reclamo = {
+        monto : 500
+    }
+    cajero.analizarReclamo(rec2)
+}
+
+mainChain()
